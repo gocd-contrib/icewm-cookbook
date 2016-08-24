052fc1c370d88a1448477d10ae543f5191052507
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-08-24 13:49:20 +0530

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'icewm'
+maintainer       'ThoughtWorks, Inc.'
+maintainer_email 'go-cd-dev@googlegroups.com'
+license          'MIT'
+description      'Installs/Configures icewm'
+version          '0.1.0'
+
+
+depends 'yum-epel'
